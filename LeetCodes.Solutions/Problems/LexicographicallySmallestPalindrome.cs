305518f705c0dc537a91f0f547df699704c418dc
Arthur Vinícius <arthurvsr@gmail.com>
2026-04-22 18:20:24 -0300

    Add solution: #2697 - Lexicographically Smallest Palindrome

---
+++ LeetCodes.Solutions/Problems/LexicographicallySmallestPalindrome.cs
@@ -0,0 +1,39 @@
+using LeetCodes.Solutions.Contract;
+
+namespace LeetCodes.Solutions.Problems
+{
+    public class LexicographicallySmallestPalindrome : ISolutionContract
+    {
+        public void Solution()
+        {
+            /*
+            2697. Lexicographically Smallest Palindrome
+            Difficulty: Easy
+            https://leetcode.com/problems/lexicographically-smallest-palindrome/
+            */
+
+            // TODO: Add your test call here, e.g.:
+            // Console.WriteLine(YourMethod(input));
+        }
+
+        public string MakeSmallestPalindrome(string s) {
+            int right = 0;
+            int left = s.Length - 1;
+
+            StringBuilder result = new(s);
+            while (right < left){
+                if (result[right] != result[left]){
+                    if (result[left] > result[right])
+                        result[left] = result[right];
+                    else
+                        result[right] = result[left];
+                }
+
+                left--;
+                right++;
+            }
+
+            return result.ToString();
+        }
+    }
+}
