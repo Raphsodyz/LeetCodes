8716ce99efae75a12412ae2b7df2f5edaf114e6c
Arthur Vinícius <arthurvsr@gmail.com>
2026-04-24 21:45:38 -0300

    Add solution: #392 - Is Subsequence

---
+++ LeetCodes.Solutions/Problems/IsSubsequence.cs
@@ -0,0 +1,37 @@
+using LeetCodes.Solutions.Contract;
+
+namespace LeetCodes.Solutions.Problems
+{
+    public class IsSubsequence : ISolutionContract
+    {
+        public void Solution()
+        {
+            /*
+            392. Is Subsequence
+            Difficulty: Easy
+            https://leetcode.com/problems/is-subsequence/
+            */
+
+            // TODO: Add your test call here, e.g.:
+            // Console.WriteLine(YourMethod(input));
+        }
+
+        public bool IsSubsequence(string s, string t) {
+            int left = 0;
+            int right = t.Length - 1;
+            Queue<char> sValues = new(s);
+
+            while(left <= right){
+                if (sValues.Count > 0 && sValues.First() == t[left])
+                    sValues.Dequeue();
+
+                left++;
+            }
+
+            if (sValues.Count == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
