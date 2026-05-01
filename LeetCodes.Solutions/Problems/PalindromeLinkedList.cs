4df5e3bfc36279c791d7e769386316ce3042069f
Arthur Vinícius <arthurvsr@gmail.com>
2026-05-01 17:42:18 -0300

    Add solution: #234 - Palindrome Linked List

---
+++ LeetCodes.Solutions/Problems/PalindromeLinkedList.cs
@@ -0,0 +1,35 @@
+using LeetCodes.Solutions.Contract;
+
+namespace LeetCodes.Solutions.Problems
+{
+    public class PalindromeLinkedList : ISolutionContract
+    {
+        public void Solution()
+        {
+            /*
+            234. Palindrome Linked List
+            Difficulty: Easy
+            https://leetcode.com/problems/palindrome-linked-list/
+            */
+
+            // TODO: Add your test call here, e.g.:
+            // Console.WriteLine(YourMethod(input));
+        }
+
+        public bool IsPalindrome(ListNode head) {
+            List<int> originalValues = new();
+            while (head != null){
+                originalValues.Add(head.val);
+                head = head.next;
+            }
+
+            List<int> reversedValues = new(originalValues);
+            reversedValues.Reverse();
+    
+            if (originalValues.SequenceEqual(reversedValues))
+                return true;
+            else
+                return false;
+        }
+    }
+}
